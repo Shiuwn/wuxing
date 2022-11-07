cff50e98cbc31f68711cc51d54c23647c1081826
wangshiyu <shiyu.wang@wengegroup.com>
2022-11-07 19:10:46 +0800

    feat: 添加速度变化函数

--- index.js
+++ index.js
@@ -1,37 +1,59 @@
 const query = (sel) => {
   return document.querySelector(sel)
 }
+const abs = (val) => Math.abs(val)
 
-const genNum = (start = 0) => () => {return (start ++) % Number.MAX_SAFE_INTEGER}
+const genNum =
+  (start = 0) =>
+  () => {
+    return start++ % Number.MAX_SAFE_INTEGER
+  }
 const genId = genNum(0)
+const genTaskId = genNum(0)
 
-const Attrs = [{
-  type: 'gold',
-  opposite: 'wood',
-  friend: 'soil'
-}, {
-
-}]
-
+const Attrs = [
+  {
+    type: 'gold',
+    opposite: 'fire',
+    friend: 'soil',
+  },
+  {
+    type: 'wood',
+    opposite: 'gold',
+    friend: 'water',
+  },
+  {
+    type: 'water',
+    opposite: 'soil',
+    friend: 'gold',
+  },
+  {
+    type: 'fire',
+    opposite: 'water',
+    friend: 'wood',
+  },
+  {
+    type: 'soil',
+    opposite: 'wood',
+    friend: 'fire',
+  },
+]
 
 class Game {
   constructor() {}
   start() {}
+  perform() {}
   stop() {}
 }
 
 class Manager {
-  collection = new WeakMap()
-  constructor() {
-  }
+  collection = []
+  constructor() {}
 
   add(dom) {
-    this.collection.set(dom, dom)
+    this.collection.push(dom)
   }
-
 }
-
-
 
 class Block {
   name = ''
@@ -45,6 +67,7 @@ class Block {
   scale = 1
   buff = 1
   dom = null
+  tasks = []
   constructor() {
     this.id = genId()
   }
@@ -73,23 +96,50 @@ class Block {
     this.scale = 1
     this.buff = 1
     this.dom = null
+    this.tasks.length = 0
     return this
   }
 
-  collide() {
-  }
+  collide() {}
 
   update() {
-    this.vx +=  this.a
+    this.tasks.slice().forEach((frame) => {
+      frame && frame.perform && frame.perform()
+    })
+    this.vx += this.a
     this.vy += this.a
     this.x += this.vx
     this.y += vy
     return this
   }
-  resize() {
-    
+  toV(v, dir = 'vx') {
+    if (abs(v, this[dir]) < this.a) return
+    const oldV = this[dir]
+    const task = {
+      id: genTaskId(),
+      perform: () => {
+        if(abs(v, this[dir]) < this.a) {
+          this.removeTask(task)
+          return
+        }
+        if (oldV > v) {
+          this[dir] = oldV - this.a
+        } else {
+          this[dir] = oldV + this.a
+        }
+      },
+    }
+    this.addTask(task)
   }
+  resize() {}
   remove() {}
+  addTask(task) {
+    this.tasks.push(task)
+  }
+  removeTask(task) {
+    const index = this.tasks.findIndex((t) => task && t.id === task.id)
+    index>-1 && this.tasks.splice(index, 1)
+  }
 }
 
 class Package {
@@ -97,27 +147,25 @@ class Package {
   attrList = []
   constructor() {}
   add(block) {
-    if(this.attrList.indexOf(block.attr)<0) {
+    if (this.attrList.indexOf(block.attr) < 0) {
       this.list.push(block)
-    }else {
+    } else {
       // end
     }
   }
   remove(block) {
-    const index = this.list.findIndex((b) =>b===block)
-    if(index>-1) {
+    const index = this.list.findIndex((b) => b === block)
+    if (index > -1) {
       this.list.splice(index, 1)
     }
   }
   swap() {
-    if(this.list.length) {
+    if (this.list.length) {
       this.list.push(this.list.shift())
     }
   }
 }
 
-function main() {
-
-}
+function main() {}
 
 main()
